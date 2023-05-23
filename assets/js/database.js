8d0373f64de4c98e138dec4b6d718f0f488bd97a
orangepi <malter-tom@web.de>
2023-05-23 22:33:55 +0200

    feat: initial database of images classified to criteria and species

---
+++ assets/js/database.js
@@ -0,0 +1,186 @@
+
+let template = {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: [],
+    german: [],
+    latin: [],
+};
+// bark: [],
+// leave: [],
+// flower: [],
+// bud: [],
+// fruit: [],
+// wood: [],
+
+let characteristic = [
+    'bark', 'leave', 'flower', 'bud', 'fruit', 'wood'
+];
+
+let species = [
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement'],
+    difficulty: 2, // 0/1/2/3
+    english: ['Korean Fir'],
+    german: ['Koreanische Tanne'],
+    latin: ['Abies Koreana'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Maple','Sycamore'],
+    german: ["Ahorn"],
+    latin: ['Acer'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['nature'],
+    difficulty: 1, // 0/1/2/3
+    english: ['Black Alder'],
+    german: ['Schwarzerle'],
+    latin: ['Alnus Glitinosa'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Birch'],
+    german: ['Birke'],
+    latin: ['Betula'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Hornbeam'],
+    german: ["Hainbuche","Weißbuche","Hagebuche"],
+    latin: ['Carpinus Betulus'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Hazelnut'],
+    german: ['Haselnuss'],
+    latin: ['Corylus'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Beech'],
+    german: ["Buche"],
+    latin: ['Fagus'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Ash'],
+    german: ['Esche'],
+    latin: ['Fraxinus Excelsior'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Walnut'],
+    german: ["Walnuss"],
+    latin: ['Juglans Regia'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 1, // 0/1/2/3
+    english: ['Aple'],
+    german: ['Apfel'],
+    latin: ['Malus'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Cherry'],
+    german: ['Süßkirsche'],
+    latin: ['Prunus Avium'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement'],
+    difficulty: 1, // 0/1/2/3
+    english: ['Plum'],
+    german: ['Pflaume'],
+    latin: ['Prunus Domestica'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 1, // 0/1/2/3
+    english: ['Bird Cherry'],
+    german: ['Traubenkirsche'],
+    latin: ['Prunus Padus'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Pear'],
+    german: ['Birne'],
+    latin: ['Pyrus'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Oak'],
+    german: ['Eiche'],
+    latin: ['Quercus'],
+  },
+
+   {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Elder'],
+    german: ['Holunder'],
+    latin: ['Sambucus Nigra'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement','nature'],
+    difficulty: 0, // 0/1/2/3
+    english: ['Lime'],
+    german: ['Linde'],
+    latin: ['Tilia'],
+  },
+
+  {
+    geoLoc: 'Middle_Europe',
+    location: ['settlement'],
+    difficulty: 2, // 0/1/2/3
+    english: ['Hemlock'],
+    german: ['Hemlocktanne','Schierlingstanne'],
+    latin: ['Tsuga'],
+  }
+];
+
+// for (i of species){console.log(i.german)}
+
